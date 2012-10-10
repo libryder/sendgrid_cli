78a5d281a665f64c463ec56946b11d83517d136f
David Ryder <david@libryder.com>
2012-10-10 00:09:26 -0700

    cleaning up tests

--- lib/sendgrid_cli/version.rb
+++ lib/sendgrid_cli/version.rb
@@ -1,3 +1,3 @@
 module SendgridCli
-  VERSION = "0.0.1"
+  VERSION = "0.1.0"
 end
--- spec/lib/spam_report_spec.rb
+++ spec/lib/spam_report_spec.rb
@@ -3,23 +3,8 @@ require 'sendgrid_cli'
 
 module SendgridCli
   describe SpamReport do
-    let(:email) { 'test@example.com' }
+    subject { SendgridCli::SpamReport.new('test@example.com', 'password') }
 
-    subject { SendgridCli::SpamReport.new(email, 'password') }
-
-    describe '#get' do
-      it 'call super with correct arugments' do
-        SendgridCli::Interface.any_instance.should_receive(:get).with('spamreports', 'get', email)
-        subject.get(email)
-      end
-    end
-
-    describe '#delete' do
-      it 'should return a successful status' do
-        SendgridCli::Interface.any_instance.should_receive(:delete).with('spamreports', 'get', email)
-        subject.delete(email)
-      end
-    end
-
+    its(:class) { should eq(SpamReport) }
   end
 end
